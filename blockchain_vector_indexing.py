bdb67dfaca1e2689f1cfb3535be5ba4a1a72e810
hpctalk <168446615+hpctalk@users.noreply.github.com>
2024-07-31 14:01:15 -0400

    Create blockchain_vector_indexing.py

---
+++ blockchain_vector_indexing.py
@@ -0,0 +1,56 @@
+from mpi4py import MPI
+import numpy as np
+import datetime      # To keep track of time, as each block has its own timestamp (exact date and time at which the block is created)
+import json          # For encoding the blocks before hashing them
+import hashlib       # For finding hashes for the blocks
+import weaviate
+import timeit
+
+class BlockchainVectorIndexing:
+    def __init__(self, V, similarity_metric_selector):
+        self.V = V
+        self.similarity_metric_selector = similarity_metric_selector
+        self.index_structure = {}  # Dictionary to store the index structure
+
+    def initialize_index(self):
+        self.index_structure = {}
+
+    def select_similarity_metric(self, vector):
+        return self.similarity_metric_selector(vector)
+
+    def compute_similarity_scores(self, vector, metric):
+        scores = {}
+        for indexed_vector in self.index_structure:
+            scores[indexed_vector] = metric(vector, indexed_vector)
+        return scores
+
+    def update_index(self, vector, similarity_scores):
+        self.index_structure[vector] = similarity_scores
+
+    def store_index_in_blockchain(self):
+        print("Storing index in blockchain:", self.index_structure)
+
+    def verify_index_integrity(self):
+        comm = MPI.COMM_WORLD
+        rank = comm.Get_rank()
+        size = comm.Get_size()
+
+        local_verification = True
+
+        results = comm.gather(local_verification, root=0)
+
+        if rank == 0:
+            if sum(results) > size / 2:
+                print("Index integrity verified")
+            else:
+                print("Index integrity verification failed")
+
+    def index_vectors(self):
+        self.initialize_index()
+
+        for vector in self.V:
+            similarity_metric = self.select_similarity_metric(vector)
+            similarity_scores = self.compute_similarity_scores(vector, similarity_metric)
+            self.update_index(vector, similarity_scores)
+            self.store_index_in_blockchain()
+            self.verify_index_integrity()
